be2f1a6c1a871cea704b0bc6811659c9512a5f2d
Aniket Agnihotri <agnihotri.aniketagnihotri.anik@gmail.com>
2024-11-27 17:33:31 +0530

    Basic Changes Made

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-     res.send('Hello World');
+    res.send('Hello World');
 });
+
 
 module.exports = app;
