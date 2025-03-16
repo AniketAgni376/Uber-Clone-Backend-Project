bed1150ba9919950d4db8aff70d586a64a1c2b5d
Aniket Agnihotri <agnihotri.aniketagnihotri.anik@gmail.com>
2025-03-16 06:27:54 +0530

    LocationSearchPanel work on progress , Home page completed

---
+++ Frontend/src/Components/LocationSearchPanel.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      {/* Sample Data */}
+    <div className='flex items-center justify-start'>
+
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-10 rounded-full'><i className='ri-map-pin-fill'></i></h2>
+        <h4 className='pl-3'>Krishna Nagar Metro Station, Terminal-1, Lucknow</h4>
+        
+    </div>
+      
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 import {useGSAP} from '@gsap/react';
 import gsap from 'gsap';
 import { useRef } from 'react';
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../Components/LocationSearchPanel';
+
+
+
 
 const Home = () => {
   const[pickup, setPickup] = useState('')
   const[destination, setDestination] = useState('')
   const[panelOpen, setPanelOpen] = useState(false)
   const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
 
 
   const submitHandler = (e) => {
@@ -18,17 +24,25 @@ const Home = () => {
   if(panelOpen){
       gsap.to(panelRef.current,{
         height: "70%", 
-        duration: 0.8, // Smooth transition duration
+        duration: 0.3, // Smooth transition duration
         ease: "back.out(1.5)", // Adds a smooth pop effect
         opacity: 1, // Ensures smooth fade-in
+        padding: 24 
+     })
+     gsap.to(panelCloseRef.current,{
+      opacity: 1, // Ensures smooth fade-in 
      })
     }else{
       gsap.to(panelRef.current,{
         height: "0%", 
-        duration: 0.8,
+        duration: 0.3,
         ease: "back.in(1.5)", // Reverse of back.out(1.5)
         opacity: 0, // Fade-out effect
       })
+      gsap.to(panelCloseRef.current,{
+        opacity: 0, // Ensures smooth fade-in 
+        padding: 0
+       })
     }
  }, [panelOpen])
     
@@ -40,7 +54,12 @@ const Home = () => {
       <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" srcSet="" />
     </div>
          <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
-         <div className='h-[30%] p-5 bg-white relative'>
+         <div className='h-[30%] p-6 bg-white relative'>
+         <h5 ref={panelCloseRef} onClick={()=>{
+            setPanelOpen(false)
+         }} className='absolute opacity-0  top-6 right-6 text-2xl'>
+          <i className="ri-arrow-down-wide-line"></i>
+          </h5>
          <h4 className='text-2xl font-semibold'>Find a trip</h4>
          <form onSubmit={(e)=>{
             submitHandler(e)
@@ -73,8 +92,8 @@ const Home = () => {
            />
          </form>
          </div>
-                     <div ref={panelRef} className='h-0 bg-red-700 '>
-
+                     <div ref={panelRef} className='h-0 bg-white'>
+                          <LocationSearchPanel />
                     </div>
               </div>
          </div>
